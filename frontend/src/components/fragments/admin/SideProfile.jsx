c813bf492c3c4f61f29e15d7761d96d29e9317d1
anwarhakim31 <anwarhakim31@gmail.com>
2024-08-01 00:59:15 +0700

    build edit prfoile sidebar

--- frontend/src/components/fragments/admin/SideProfile.jsx
+++ frontend/src/components/fragments/admin/SideProfile.jsx
@@ -1,15 +1,25 @@
 import { selectedUserData } from "@/store/slices/auth-slice";
-import { User } from "lucide-react";
+import { Edit2, Plus, User } from "lucide-react";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { useSelector } from "react-redux";
 
 const SideProfile = () => {
   const data = useSelector(selectedUserData);
-  const [setHover, setIsHover] = useState(false);
+  const [isHover, setIsHover] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   return (
     <div className="fixed p-4 rounded-md shadow-lg top-1/2 -translate-y-1/2 right-0 w-[204px] h-[60vh] bg-white">
-      <caption className="relative cursor-pointer w-16 h-16 mx-auto bg-backup flex items-center justify-center rounded-full overflow-hidden">
+      <caption
+        onMouseEnter={() => setIsHover(true)}
+        onMouseLeave={() => setIsHover(false)}
+        className="relative cursor-pointer w-16 h-16 mx-auto bg-backup flex items-center justify-center rounded-full overflow-hidden"
+      >
         {data.photo ? (
           <img src={data.photo} alt="foto" />
         ) : (
@@ -20,9 +30,23 @@ const SideProfile = () => {
             className="mt-7 fill-white"
           />
         )}
-        <div className="absolute inset-0 bg-black/20"></div>
+        {isHover && (
+          <div className="absolute inset-0 bg-black/30 w-full h-full flex-center">
+            {data.image ? (
+              <Plus color="white" width={20} height={20} />
+            ) : (
+              <Edit2 color="white" width={20} height={20} />
+            )}
+          </div>
+        )}
         <input type="file" className="hidden" />
       </caption>
+      <form
+        action="
+          "
+      >
+        <input type="text" />
+      </form>
     </div>
   );
 };
