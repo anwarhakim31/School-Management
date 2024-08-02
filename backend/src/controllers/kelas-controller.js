67ed5180aca49f289a59bcc01a79ed07b577d1f4
anwarhakim31 <anwarhakim31@gmail.com>
2024-08-02 16:34:30 +0700

    create crud kelas

--- backend/src/controllers/kelas-controller.js
+++ backend/src/controllers/kelas-controller.js
@@ -18,3 +18,60 @@ export const addKelas = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getKelas = async (req, res, next) => {
+  try {
+    const kelas = await Kelas.find();
+
+    res.status(200).json({
+      success: true,
+      message: "Berhasil mengambil data kelas.",
+      kelas,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteKelas = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const result = await Kelas.findByIdAndDelete(id);
+
+    if (!result) {
+      throw new ResponseError(404, "Kelas tidak di temukan");
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Kelas berhasil di hapus.",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateKelas = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const updateData = req.body;
+
+    const kelas = await Kelas.findByIdAndUpdate(
+      id,
+      { $set: updateData },
+      { runValidators: true, new: true }
+    );
+
+    if (!kelas) {
+      throw new ResponseError(404, "Kelas tidak di temukan");
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Kelas berhasil di ubah.",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- backend/src/routes/kelas-router.js
+++ backend/src/routes/kelas-router.js
@@ -1,9 +1,17 @@
 import express from "express";
 import verifyToken from "../middlewares/auth-middleware.js";
-import { addKelas } from "../controllers/kelas-controller.js";
+import {
+  addKelas,
+  deleteKelas,
+  getKelas,
+  updateKelas,
+} from "../controllers/kelas-controller.js";
 
 const kelasRouter = express.Router();
 
 kelasRouter.post("/add-kelas", verifyToken, addKelas);
+kelasRouter.get("/get-kelas", verifyToken, getKelas);
+kelasRouter.delete("/delete-kelas/:id", verifyToken, deleteKelas);
+kelasRouter.put("/update-kelas/:id", verifyToken, updateKelas);
 
 export default kelasRouter;
